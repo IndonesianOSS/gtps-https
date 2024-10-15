172d4cc2871b48e5479007f975d5f650766a3251
VernSG <snexmania76@gmail.com>
2024-10-15 23:22:47 +0800

    feat: add limiter for antiddos using helmet and express-rate-limit

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-const express = require('express');
-const https = require('https');
-const app = express();
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
-const privateKey = fs.readFileSync('key.pem', 'utf8');
-const certificate = fs.readFileSync('cert.pem', 'utf8');
+const app = express();
+app.use(helmet());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+});
+app.use(limiter);
+
+const privateKey = fs.readFileSync("key.pem", "utf8");
+const certificate = fs.readFileSync("cert.pem", "utf8");
 const credentials = { key: privateKey, cert: certificate };
 
-// Log function
-
 const logConnection = (req, sentKB) => {
-    const log = `--------------\n${new Date().toISOString()} || Connection from: ${req.ip}. Connected to ${req.path}. Sended: ${sentKB}kb.\n--------------`;
-    fs.appendFile(path.join(__dirname, 'log.txt'), log + '\n', (err) => {
-        if (err) throw err;
-    });
-    console.log(log);
+  const log = `--------------\n${new Date().toISOString()} || Connection from: ${
+    req.ip
+  }. Connected to ${req.path}. Sent: ${sentKB} KB.\n--------------`;
+  fs.appendFile(path.join(__dirname, "log.txt"), log + "\n", (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
+  console.log(log);
 };
 
-// Function to read the JSON file and return the object
 function getJsonObjectFromFile(fileName, key) {
-    try {
-        const data = fs.readFileSync(fileName, 'utf8');
-        const jsonData = JSON.parse(data);
-        return jsonData[key];
-    } catch (error) {
-        console.error('Error reading the JSON file:', error);
-        return null;
-    }
+  try {
+    const data = fs.readFileSync(fileName, "utf8");
+    const jsonData = JSON.parse(data);
+    return jsonData[key];
+  } catch (error) {
+    console.error("Error reading the JSON file:", error);
+    return null;
+  }
 }
 
-//get the conf
-const conf = getJsonObjectFromFile('conf.json', 'config');
+const conf = getJsonObjectFromFile("conf.json", "config");
 
 var pack = `server|${conf.ip}
 port|${conf.port}
@@ -46,72 +55,59 @@ meta|localhost
 RTENDMARKERBS1001
 `;
 
-// Middleware to log connections
 app.use((req, res, next) => {
-    const startTime = new Date();
-    res.on('finish', () => {
-        const endTime = new Date();
-        const sentKB = res.get('Content-Length') / 1000 || 0;
-        logConnection(req, sentKB);
-    });
-    next();
+  const startTime = new Date();
+  res.on("finish", () => {
+    const endTime = new Date();
+    const sentKB = res.get("Content-Length") / 1000 || 0;
+    logConnection(req, sentKB);
+  });
+  next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Sample route
-app.get('/', (req, res) => {
-    res.send('NaN');
+app.post("/growtopia/server_data.php", (req, res) => {
+  res.send(pack);
 });
 
-//server_data.php
-app.post('/growtopia/server_data.php', (req,res) => {
-    res.send(pack);
-});
-//Hosted host for powertunnel
-app.get('/host', (req, res) => {
-    if (conf) {
-        res.send(`#Powertunnel enjoyer : )<br>${conf.ip} www.growtopia1.com<br>${conf.ip} www.growtopia2.com
-        `);
-    } else {
-        res.status(500).send('Internal Server Error');
-    }
+app.get("/host", (req, res) => {
+  if (conf) {
+    res.send(
+      `#Powertunnel enjoyer : )<br>${conf.ip} www.growtopia1.com<br>${conf.ip} www.growtopia2.com`
+    );
+  } else {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-// Route for downloading the file
-app.get('/downhost', function (req, res) {
-    const filePath = path.join(__dirname, 'public', 'host.txt');
+app.get("/downhost", function (req, res) {
+  const filePath = path.join(__dirname, "public", "host.txt");
 
-    // Sending the file as a response with a custom filename
-    const fileName = 'host.txt';
-    res.sendFile(filePath, {
-        headers: {
-            'Content-Disposition': `attachment; filename="${fileName}"`,
-        },
-    }, function (err) {
-        if (err) {
-            if (err.code === 'ECONNABORTED') {
-                console.log('Request aborted by the client');
-            } else {
-                console.error('Error while sending file:', err);
-                res.status(err.status).end();
-            }
+  res.sendFile(
+    filePath,
+    {
+      headers: {
+        "Content-Disposition": `attachment; filename="host.txt"`,
+      },
+    },
+    function (err) {
+      if (err) {
+        if (err.code === "ECONNABORTED") {
+          console.log("Request aborted by the client");
         } else {
-            console.log('File sent successfully');
+          console.error("Error while sending file:", err);
+          res.status(err.status).end();
         }
-    });
+      } else {
+        console.log("File sent successfully");
+      }
+    }
+  );
 });
 
-// Starting the express server on port 80
-const HTTP_PORT = 80;
-app.listen(HTTP_PORT, () => {
-    console.log(`Server is running on http://localhost:${HTTP_PORT}`);
-});
-
-
-// Starting the HTTPS server on port 443
-const httpsServer = https.createServer(credentials, app);
 const HTTPS_PORT = 443;
+const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(HTTPS_PORT, () => {
-    console.log(`HTTPS Server is running on https://localhost:${HTTPS_PORT}`);
+  console.log(
+    `HTTPS Server for game is running on https://localhost:${HTTPS_PORT}`
+  );
 });
